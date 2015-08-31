822d486b254cf1944b1d6a3f57301cae5e91a40b
root <root@localhost.localdomain>
2015-08-31 16:43:04 +0800

    commit all file of data

--- src/data_app.erl
+++ src/data_app.erl
@@ -3,14 +3,22 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/2,start/0,stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
+start() ->
+    start([],[]).
 start(_StartType, _StartArgs) ->
-    data_sup:start_link().
+    io:format("application starting~n"),
+    case data_sup:start_link() of
+        {ok,Pid} ->
+            {ok,Pid};
+        Other    ->
+            {error,Other}
+    end.
 
 stop(_State) ->
     ok.
--- src/data_sup.erl
+++ src/data_sup.erl
@@ -10,18 +10,21 @@
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
+-define(SERVER,?MODULE).
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+    Server = {data_server,{data_server,start_link,[]},
+              permanent,2000,worker,[tr_server]},
+    Children = [Server],
+    Restart = {one_for_one,0,1},
+    {ok,{Restart,Children}}.
